53cd61d2121e83c1e0acecae8763382eda8bd0da
mosqlee <liweiwen92@163.com>
2018-05-05 17:51:47 +0800

    u111

--- nodeApi/awsS3.js
+++ nodeApi/awsS3.js
@@ -2,13 +2,25 @@ const crypto = require('crypto');
 
 const secret = 'i1kTH2JZ63dAzhxxwcBppyRAodkzBOqFSCf3NOml';
 
-const POLICY_JSON = {
-  "expiration": "2020-12-01T12:00:00.000Z",
+let POLICY_JSON = {
+  "expiration": "2022-12-01T12:00:00.000Z",
   "conditions": [
-    { "bucket": this.get('bucket') },
-    ["starts-with", "$key", ""],
-    { "acl": this.get('acl') },
+    { "bucket": "moztest" },
+    ["starts-with", "$key"],
+    { "acl": "public-read" },
     ["starts-with", "$Content-Type", ""],
     ["content-length-range", 0, 524288000]
   ]
 };
+function getSign(policy, secret) {
+  const policyBase64 = Buffer.from(JSON.stringify(policy)).toString('base64');
+  const signature = crypto.createHmac('sha1', secret).update(policyBase64).digest().toString('base64');
+  return { signature, policyBase64 };
+}
+export default function apiToken(req, res) {
+  let { folder, filer, contentType } = req.data;
+  const key = folder + filer;
+  POLICY_JSON["conditions"][1].push(key);
+  POLICY_JSON["conditions"][3].push(contentType);
+  return res.json('200', getSign(POLICY_JSON, secret));
+}
